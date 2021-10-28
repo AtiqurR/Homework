e5c1f93068175795cfd8f46d20befda82350e765
AtiqurR <atqrrhman@gmail.com>
2021-10-27 20:03:34 -0400

    10272021 8:03 pm

---
+++ src/hw4Q2Method/Month.java
@@ -0,0 +1,55 @@
+package hw4Q2Method;
+
+public class Month {
+
+	public static void main(String[] args) {
+		String MonthOfBirth="March";
+		
+		switch (MonthOfBirth) {
+		
+		case "January" :
+			System.out.println("Your month of birth January");
+			break;
+		case "February" :
+			System.out.println("Your month of birth February");
+			break;
+		case "March":
+            System.out.println("Your month of birth March");
+            break;
+		case "April" :
+			System.out.println("Your month of birth April");
+			break;
+		case "May" :
+			System.out.println("Your month of birth April");
+			break;
+		case"June" :
+			System.out.println("Your month of birth June");
+			break;
+		case "July" :
+			System.out.println("Your month of birth June");
+			break;
+		case "August" :
+			System.out.println("Your month of birth August");
+			break;
+		case "September" :
+			System.out.println("Your month of birth September");
+			break;
+		case "October" :
+			System.out.println("Your month of birth October");
+			break;
+		case "November" :
+			System.out.println("Your month of birth November");
+			break;
+		case "December" :
+			System.out.println("Your month of birth December");
+			break;
+			
+			default :
+				System.out.println("Sorry we don't have your month of birth");
+		
+		}
+		
+
+	}
+
+}
